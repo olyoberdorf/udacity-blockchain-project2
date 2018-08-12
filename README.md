86bc4fc2f98d4b91e752379e6c8fa001e9e2c8a7
Oliver Oberdorf <oly@oberdorf.org>
2018-08-12 08:00:55 -0400

    Forgot to commit these - lazy initialized version of my blockchain

--- README.md
+++ README.md
@@ -26,15 +26,34 @@ I took the following liberties in designing my blockchain code.
  2. The `getBlockHeight` call returns the height of the chain, but it is using
     the cached value from the initial load (or computed from subsequent
     `addBlock` calls).  This is more efficient than reading data from `levelDB`
-    every time.  However, the cached value is computed during a load from
-    `levelDB` in `initFromLevelDB()` and I feel this meets the intent of the
-    project.
- 3. I included logic to load a chain on startup.  This probably is not what is
+    every time.
+ 3. I included logic to load on subsequent runs.  This probably is not what is
     intended for the test, but seems like an obvious feature to include.  So,
     to test a fresh chain simply delete the `chaindata` folder prior to running.
     If you run without deleting the chain, it will use the chain as is.  Note
     that if the chain was generated via the test code as I have it, it will
     already have the induced errors (though it also induces them again).
+ 4. The optional chain load mentioned in (3) is handled via a lazy initialization
+    routine.  This routine is at the start of every member call in the
+    Blockchain class.  This was my solution to the earlier project feedback.
+    The lazy initialization will also create the database f
+
+## Resubmit
+
+This is a resubmission of the project.  The reviewer objected to my use of a
+separate initialization call after the constructor.  I am not sure exactly
+what they are looking for.  It seems like there are only a few options:
+
+ 1. Provide a simple constructor and an async init method as I had, and use it.
+ 2. Provide a reentrant lazy init method as a member of the class and call it at
+    the start of all other methods.
+ 3. Provide a factory async method that does the load/create of leveldb, then
+    passes the necessary state variables into the Blockchain constructor.  It
+    could then resolve to the constructed Blockchain.
+
+I am resubmitting with approach 2.  If I misunderstood the reason for my
+rejection, I'd appreciate guidelines accepting/rejecting these three proposals
+so I know what the reviewers consider a valid architecture.
 
 ## Testing
 
@@ -45,4 +64,4 @@ To test code:
 
 The test logic appears at the end of `simpleChain.js` so it will automatically
 execute.  It follows the description from the project 2 readme, with the
-caveats listed in *Notes* above.
+caveats listed in *Notes* and *Resubmit* above.
--- simpleChain.js
+++ simpleChain.js
@@ -13,6 +13,7 @@ const level = require('level');
 const chainDB = './chaindata';
 const db = level(chainDB);
 
+
 /*****************************************************************
  * Utility method to add string data to LevelDB with a given key.
  *
@@ -20,7 +21,7 @@ const db = level(chainDB);
  * prior to continuing execution, use the returned Promise to
  * manage that.
  *****************************************************************/
-async function addLevelDBDataAsync(key,value){
+async function addLevelDBStringAsync(key,value){
   return new Promise(function(resolve, reject) {
       db.put(key, value, function(err) {
         if (err) {
@@ -34,22 +35,46 @@ async function addLevelDBDataAsync(key,value){
 }
 
 /**************************************************
- * Utility method to get data from LevelDB by key.
+ * Utility method to get string data from LevelDB by key.
  *
  * This method is async, so the caller should
  * access the return via the Promise.
  **************************************************/
-async function getLevelDBDataAsync(key){
+async function getLevelDBStringAsync(key){
   return new Promise(function(resolve, reject) {
     db.get(key, function(err, result) {
       if (err == undefined) {
-        resolve(JSON.parse(result));
+        resolve(result);
       } else {
         reject(err);
       }
     });
   });
 }
+
+/*****************************************************************
+ * Utility method to add JSON data to LevelDB with a given key.
+ *
+ * This method is async.  If you need to know the data was saved
+ * prior to continuing execution, use the returned Promise to
+ * manage that.
+ *****************************************************************/
+async function addLevelDBJSONAsync(key,value) {
+  return addLevelDBStringAsync(key, JSON.stringify(value));
+}
+
+/**************************************************
+ * Utility method to get JSON data from LevelDB by key.
+ *
+ * This method is async, so the caller should
+ * access the return via the Promise.
+ **************************************************/
+async function getLevelDBJSONAsync(key){
+  let svalue = await getLevelDBStringAsync(key);
+  let value = JSON.parse(svalue);
+  return value;
+}
+
 
 // -----
 
@@ -72,78 +97,163 @@ class Block{
 |  ================================================*/
 
 class Blockchain{
-  constructor(){
+  /**************************
+   * Constructor for Blockchain
+   *
+   * height and previousBlockHash are used to cache those aspects of the
+   * blockchain state.  Notice that all member methods call a lazy initialization
+   * method prior to their contents.  These values will be set by that lazy
+   * initializer prior to being accessed by any real functionality.
+   *************************************************************************/
+  constructor() {
     this.height = -1;
     this.previousBlockHash = "";
   }
 
-  // Add new block
-  async addBlock(newBlock){
-    this.height = this.height+1;
-    // Block height
-    newBlock.height = this.height;
+  /***************************************************************************
+   * Perform the actual Database initialization.
+   *
+   * This call is used if we have to initialize the leveldb database.  We
+   * write a height of 0 for the genesis block, then we create and add it
+   * at the key of 0.  Then we write a version of "1.0".  Once the version
+   * is in leveldb, future runs will consider the database valid and not
+   * delete the old chain.
+   **************************************************************************/
+  async _initDatastore() {
+    await addLevelDBStringAsync("height", "0");
+    let block = new Block("First block in the chain - Genesis block");
+    // using internal method so it doesn't call _initDatastore() again
+    this._addBlock(block);
+    // finally set the version, now that the DB state is clean
+    await addLevelDBStringAsync("version", "1.0");
+  }
+
+  /**************************************************************************
+   * This is a lazy initialization routine used by all member methods
+   * of the Blockchain.
+   *
+   * Prior to executing the body of other methods, this is always called to
+   * ensure we are initialized.  The initialization is kept ouf of the
+   * constructor because it relies on async levelDB calls and we want a
+   * clean and fast constructor.
+   *
+   * If the cached height of the block chain is no longer -1, we infer that
+   * the chain has been loaded or initialized already and this becomes a
+   * no-op.
+   *
+   * If there is no "version" value in levelDB, or if it is not set to "1.0",
+   * you have an unrecognized chain and we replace it.  Similarly, if the
+   * chain DB did not exist, we will get the NotFoundError on version and
+   * initialize it too.
+   *
+   * Other error types are thrown up the call stack.
+   **************************************************************************/
+  async _checkInit() {
+    // TODO investigate babel and annotations to use this, rather than directly
+    debug('checking if we need to initialize');
+    if (this.height < 0) {
+      try {
+        debug('requesting version from DB');
+        let version = await getLevelDBStringAsync("version");
+        debug('got version of ' + version);
+        if (version !== "1.0") {
+          debug("Unrecognized version in leveldb, initializing database fresh");
+          await this._initDatastore();
+        }
+      } catch (err) {
+        if (err.type === "NotFoundError") {
+          debug("No version value found in leveldb, initializing database fresh");
+          await this._initDatastore();
+        } else {
+          console.error("Error checking for version in levelDB");
+          throw err;
+        }
+      }
+      try {
+        debug('reading height of chain from DB');
+        let sheight = await getLevelDBStringAsync("height");
+        let height = parseInt(sheight);
+        debug('got height of ' + height);
+        this.height = height;
+        debug('getting block at ' + height + ' to read the final hash');
+        let block = await this.getBlock(this.height);
+        this.previousBlockHash = block.hash;
+        debug('all done, saved final hash of ' + this.previousBlockHash);
+      } catch (err) {
+        console.error("Error reading height/block data to initialize");
+        throw err;
+      }
+    }
+  }
+
+  /********************************************************************
+   * Internal add block method.
+   *
+   * This adds blocks to the chain but it omits two steps handled by
+   * the public addBlock() call, below.  It does not set the height of
+   * the new block and it does not set the previous block hash on the
+   * new block.  This allows us to use this during initialization when
+   * we want to explicitly add the genesis block with a fixed height/
+   * previous hash value.
+   ********************************************************************/
+  async _addBlock(newBlock){
     // UTC timestamp
     newBlock.time = new Date().getTime().toString().slice(0,-3);
-    // previous block hash
-    newBlock.previousBlockHash = this.previousBlockHash;
 
     // Block hash with SHA256 using newBlock and converting to a string
     newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
 
 
-    this.previousBlockHash = newBlock.hash;
-
     debug("adding block %s", JSON.stringify(newBlock));
-  	  return await addLevelDBDataAsync(newBlock.height, JSON.stringify(newBlock));
+  	await addLevelDBJSONAsync(newBlock.height, newBlock);
+    await addLevelDBStringAsync("height", newBlock.height);
+
+    if (newBlock.height > this.height) {
+      this.height = newBlock.height;
+    }
+    this.previousBlockHash = newBlock.hash;
   }
 
-    // Get block height
-    getBlockHeight(){
-      // we cache and maintain the height at all times, so this is easy
+/********************************************************************
+ * Add block to the chain.
+ *
+ * This is the publicly used call to add blocks to the chain.  In addition
+ * to saving it into levelDB, it also:
+ *   1. Sets the height to the existing chain height +1, this adds it to the end
+ *   2. Sets the previous block hash to the cached hash of the last added block
+ * During the call to _addBlock, these values will factor into the SHA256
+ * calculation and on successfully adding to levelDB, the cached height
+ * and previousBlockHash values for the chain will be updated.
+ ********************************************************************/
+  async addBlock(newBlock) {
+    newBlock.height = this.height+1;
+    newBlock.previousBlockHash = this.previousBlockHash;
+    await this._checkInit();
+    await this._addBlock(newBlock);
+  }
+
+    /*****************************************************************
+     * Get the block height of the chain.
+     *
+     * The block height of the chain is cached.  After the init call
+     * finishes, we should have a cached height to return.  The init
+     * call is at the start of all the member methods since we cannot
+     * reasonably add it to the constructor.
+     *****************************************************************/
+    async getBlockHeight() {
+      debug('getting block height of chain');
+      await this._checkInit();
       return this.height;
     }
 
-    // get block
-    async getBlock(blockHeight) {
-      return getLevelDBDataAsync(blockHeight);
-    }
-
-    /***********************************************************
-     * Initialize the blockchain from levelDB.
+    /**********************************************************************
+     * Get a block.
      *
-     * This method takes an argument, but it should be called
-     * without one (default of 0).  The method is recursive
-     * and after loading each block, it will increment the
-     * block height and try to load the next one.  Eventually,
-     * there will be no block (a "NotFoundError" in leveldb)
-     * and we know we've reached the end of the chain.
-     ************************************************************/
-    async initFromLevelDB(blockHeight=0) {
-      // note - recursive
-      // TODO: tail recursion
-      if (blockHeight == 0) {
-        debug('initializing chain from leveldb');
-      }
-      try {
-        let block = await this.getBlock(blockHeight);
-        this.height = blockHeight;
-        this.previousBlockHash = block.hash;
-        return await this.initFromLevelDB(blockHeight+1);
-      } catch(err) {
-        if (err.type === "NotFoundError") {
-	        // we read all blocks, if height is -1 there was no data, so add a genesis block
-	        if (this.height == -1) {
-	          // no data in levelDB, initialize genesis block
-	          debug('no chain found in leveldb, initializing a new one with a genesis block');
-	          return await this.addBlock(new Block("First block in the chain - Genesis block"));
-	        } else {
-	          debug('initialized from leveldb, height is %s', this.height);
-	        }
-        } else {
-          console.error("Error during chain init from leveldb: " + err);
-          throw err;
-        }
-      }
+     * This gets a block out of leveldb by it's height.
+     *********************************************************************/
+    async getBlock(blockHeight) {
+      await this._checkInit();
+      return getLevelDBJSONAsync(blockHeight);
     }
 
 
@@ -154,8 +264,9 @@ class Blockchain{
      * fully formed block as an argument.  It does not validate the chain
      * around the block (i.e. previousHash) - only the block itself.
      *********************************************************************/
-    validateBlock(block){
+    async validateBlock(block){
       debug("validating block %s", JSON.stringify(block));
+      await this._checkInit();
       // get block hash
       let blockHash = block.hash;
       let hashableBlock = Object.assign({}, block);
@@ -171,41 +282,6 @@ class Blockchain{
         }
     }
 
-    /***************************************************************************
-     * Validate the chain.
-     *
-     * This call is also recursive.  It should be started by calling it without
-     * arguments.  It will begin on the first block and recursively call
-     * itself for the subsequent blocks until it hits the block height.
-     *
-     * Errors are added to a list and returned at the end of the recursion.
-     ***************************************************************************/
-    async validateChainAt(i=0, errorLog = []) {
-        // note: recursive
-        // TODO: use tail recursion
-        if (i==0) {
-          debug("validating the chain");
-        }
-        let block = await this.getBlock(i);
-
-        // validate block
-        if (!(this.validateBlock(block))) errorLog.push(i);
-        // compare blocks hash link
-        if (i<this.height) {
-          let blockHash = block.hash;
-	        let nextBlock = await this.getBlock(i+1);
-	        let previousHash = nextBlock.previousBlockHash;
-	        if (blockHash!==previousHash) {
-	          console.log("block " + i + " hash: " + block.hash + " does not match next previous hash of " + nextBlock.previousBlockHash);
-	          errorLog.push(i);
-	        }
-	        return await this.validateChainAt(i+1, errorLog);
-	      } else {
-	        console.log("detected end of chain at block #" + i);
-	        return errorLog;
-	      }
-    }
-
     /*************************************************************
      * Validate the chain.
      *
@@ -214,19 +290,39 @@ class Blockchain{
      * individual blocks and the chain of hash references.  It
      * returns the list of errors, if any, as a Promise.
      *************************************************************/
-    async validateChain(){
-      let previousHash = '';
-      var i = 0;
-      let errorLog = await this.validateChainAt();
-      if (errorLog.length>0) {
-	      console.log('Block errors = ' + errorLog.length);
-	      console.log('Blocks: '+errorLog);
-	    } else {
-	      console.log('No errors detected');
-	    }
-	    return;
+    async validateChain() {
+        await this._checkInit();
+
+        debug("validating the chain");
+
+        let errorLog = [];
+
+        for (var i=0; i<=this.height; i++) {
+          let block = await this.getBlock(i);
+
+          // validate block
+          if (!(await this.validateBlock(block))) errorLog.push(i);
+          // compare blocks hash link
+          if (i<this.height) {
+            let blockHash = block.hash;
+	          let nextBlock = await this.getBlock(i+1);
+	          let previousHash = nextBlock.previousBlockHash;
+	          if (blockHash !== previousHash) {
+	            console.log("block " + i + " hash: " + block.hash + " does not match next previous hash of " + nextBlock.previousBlockHash);
+	            errorLog.push(i);
+	          }
+          }
+	      }
+        if (errorLog.length>0) {
+  	      console.log('Block errors = ' + errorLog.length);
+  	      console.log('Blocks: '+errorLog);
+  	    } else {
+  	      console.log('No errors detected');
+  	    }
+        return errorLog;
     }
 }
+
 
 // =============================================================================
 
@@ -234,9 +330,8 @@ class Blockchain{
  * Beginning of test code for simpleChain.js
  ********************************************/
 
-// testing
-debug('creating blockchain');
-let blockchain = new Blockchain();
+ let blockchain = new Blockchain();
+
 
 /***********************************************************
  * Helper method to load the chain.
@@ -247,22 +342,24 @@ let blockchain = new Blockchain();
  ***********************************************************/
 async function testLoadBlockchain() {
   debug('initializing from leveldb');
-  return blockchain.initFromLevelDB().then(function() {
-	  if (blockchain.height <= 1) {
-	     let thenables = [];
-		  for (var i=0; i<=10; i++) {
-		    thenables.push(blockchain.addBlock(new Block("test data " + i)));
-		  }
-		  return Promise.all(thenables);
-	  } else {
-	    return console.error('Found existing blockchain in levelDB, using that.  Is this what you wanted?  (see test code in simpleChain.js)');
-	  }});
+
+  let height = await blockchain.getBlockHeight();
+  if (height <= 1) {
+     debug('looks like a fresh chain, adding 10 blocks');
+     let thenables = [];
+	   for (var i=0; i<=10; i++) {
+	     await blockchain.addBlock(new Block("test data " + i));
+	   }
+  } else {
+     console.error('Found existing blockchain in levelDB, using that.  Is this what you wanted?  (see test code in simpleChain.js)');
+  }
 }
 
 /**************************************
  * Helper method to validate the chain
  **************************************/
 async function testValidateChain() {
+  debug('testing valid chain');
   return blockchain.validateChain();
 }
 
@@ -276,15 +373,16 @@ async function testValidateChain() {
  * validate.
  **********************************************/
 async function testInduceErrors() {
-      let inducedErrorBlocks = [2,4,7];
-      thenables = [];
-      for (var i = 0; i < inducedErrorBlocks.length; i++) {
-        blockchain.getBlock(inducedErrorBlocks[i]).then(function(block) {
-          block.data = 'induced chain error';
-          thenables.push(addLevelDBDataAsync(block.height, JSON.stringify(block)));
-        });
-      }
-      return Promise.all(thenables);
+  debug('test logic inducing errors at blocks 2,4,7');
+  let inducedErrorBlocks = [2,4,7];
+  thenables = [];
+  for (var i = 0; i < inducedErrorBlocks.length; i++) {
+    blockchain.getBlock(inducedErrorBlocks[i]).then(function(block) {
+      block.data = 'induced chain error';
+      thenables.push(addLevelDBJSONAsync(block.height, block));
+    });
+  }
+  return Promise.all(thenables);
 }
 
 /*****************************************************************************
@@ -295,7 +393,10 @@ async function testInduceErrors() {
  * 3. Induce errors in blocks 2, 4, and 7
  * 4. Validate the chain, it should fail and complain about blocks 2, 4, and 7
  *****************************************************************************/
-testLoadBlockchain(blockchain)
-.then(testValidateChain)
-.then(testInduceErrors)
-.then(testValidateChain)
+
+debug("Initializing from LevelDB");
+
+testLoadBlockchain()
+  .then(testValidateChain)
+  .then(testInduceErrors)
+  .then(testValidateChain);
